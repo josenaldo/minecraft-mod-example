9afd0ad826e11b452c15c6f867982e5332eaff7e
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2020-08-09 20:31:56 -0300

    Bloco agora tem visual e pode ser usado com item

--- src/main/java/br/com/vudureverso/vuduhopper/VuduHopperMod.java
+++ src/main/java/br/com/vudureverso/vuduhopper/VuduHopperMod.java
@@ -4,6 +4,9 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -16,8 +19,11 @@ public class VuduHopperMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		System.out.println("*****************************");
-		System.out.println("Inicializando mod Vudu Hopper");
+		System.out.println("**** Registrando bloco vudu hopper ****");
 		Registry.register(Registry.BLOCK, new Identifier("vudureverso", "vuduhopper"), VUDU_HOPPER_BLOCK);
+
+		System.out.println("**** Registrando bloco vudu hopper como item ****");
+		Registry.register(Registry.ITEM, new Identifier("vudureverso", "vuduhopper"), new BlockItem(VUDU_HOPPER_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
+
 	}
 }
